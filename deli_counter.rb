0558153a88f038675f8459c652d70c677679162d
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-04 17:54:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,10 @@ end
 
 def now_serving(katz_deli)
   statement = "Now serving number: "
+  katz_deli.first = first_in_line
   if katz_deli.empty?
     puts "There is nobody waiting to be served."
   else
-    katz_deli.first
+    puts statement + #{first_in_line}
+    
   
