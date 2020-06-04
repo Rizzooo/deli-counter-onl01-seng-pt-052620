1882e300062d8b3eeba39630425b70768a75e4f1
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-04 17:38:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ end
 
 def take_a_number(katz_deli, name)
     katz_deli.push(name)
+    katz_deli.length = 
     
     puts "Welcome, " + name + ". You are number" + katz_deli.length + "in line."
  end
